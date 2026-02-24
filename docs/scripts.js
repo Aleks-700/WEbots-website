b9d3ae253efe53d0a103903e7c255ffd7c750006
Aleks-700 <ajamina@uwo.ca>
2026-02-23 22:33:00 -0500

    Mobile Layout Fix 1

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,20 +1,17 @@
 window.addEventListener('scroll', reveal);
 
-  function reveal(){
-    var reveals = document.querySelectorAll('.reveal');
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
 
-    for(var i=0; i< reveal.length; i++){
-      var windowheight = window.innerHeight;
-      var revealtop = reveals[i].getBoundingClientRect().top;
-      var revealpoint =150;
+  for (var i = 0; i < reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
 
-      if(revealtop < windowheight - revealpoint){
-        reveals[i].classList.add('active');
-
-      }
-      else{
-        reveals[i].classList.remove('active');
-      }
+    if (revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
     }
   }
-
+}
